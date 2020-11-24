45bec6d15914dde9c7a0f37ec11789ad370eace1
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-24 10:23:30 +0200

    extending methods example

--- MethodExample.cs
+++ MethodExample.cs
@@ -1,26 +1,64 @@
 ﻿using System;
 
-namespace _01.usingMethods
+namespace _02.usingMethods
 {
-    class MethodExample
+    class Program
     {
-        static void PrintLogo()
+        static void PrintSign(int number)
         {
-            Console.WriteLine("Telerik corp.");
-            Console.WriteLine("www.telerik.com");
-            //this is a method
+            if (number > 0)
+            {
+                Console.WriteLine("The number {0} is positive.", number);
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("The number {0} is negative .", number);
+            }
+            else
+            {
+                Console.WriteLine("The numebr {0} is zero.", number);
+            }
         }
-        static void PrintAcademyLogo() 
-        {
-            Console.WriteLine("Telerik Academy");
-            Console.WriteLine("Part Of");
-            PrintLogo(); //We call he the first Method PrintLogo();
-        }
-        static void Main() 
-        {
-            PrintAcademyLogo();
-            //this is a invoke of a method, callint a method!
 
+
+        static void PrintMax(float number1, float number2)
+        {
+            float max = number1;
+            if (number2 > number1)
+            {
+                max = number2;
+            }
+            Console.WriteLine("Maximal  number :{0}", max);
         }
+
+        static void MakeNumberPositive(int number)
+        {
+            if (number < 0)
+            {
+                number = -number; //We reinitialize the same numbber with minus sign in front
+                                  //i e, as the number is negative then we make it positive : -(-) = +;
+
+            }
+            Console.WriteLine(number);
+        }
+        static void ChangeFirstElement(int[] arr)
+        {
+            arr[0] = 42;
+        }
+
+
+        static void Main()
+        {
+            int n = -5;
+            MakeNumberPositive(n);
+            Console.WriteLine(n);
+
+            int[] array = new int[] { 1, 2, 3 };
+
+            Console.WriteLine(array[0]);
+            ChangeFirstElement(array);
+            Console.WriteLine(array[0]);
+        }
+
     }
 }
