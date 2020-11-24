54eb74e6ee4dd758c443cbcb499cbbb2ffb30a30
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-24 08:05:32 +0200

    init

---
+++ MethodExample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01.usingMethods
+{
+    class MethodExample
+    {
+        static void PrintLogo()
+        {
+            Console.WriteLine("Telerik corp.");
+            Console.WriteLine("www.telerik.com");
+            //this is a method
+        }
+        static void PrintAcademyLogo() 
+        {
+            Console.WriteLine("Telerik Academy");
+            Console.WriteLine("Part Of");
+            PrintLogo(); //We call he the first Method PrintLogo();
+        }
+        static void Main() 
+        {
+            PrintAcademyLogo();
+            //this is a invoke of a method, callint a method!
+
+        }
+    }
+}
